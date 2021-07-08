ae9754d0786eb558621f35362a98dd45753666fb
lidiiakuznetsova <76099410+lidiiakuznetsova@users.noreply.github.com>
2021-07-08 15:25:41 +0300

    Create app.js

---
+++ TODOList/app.js
@@ -0,0 +1,43 @@
+let buttonEnter = document.getElementById('enter')
+let userInput = document.getElementById('userInput')
+let ul = document.querySelector('ul')
+
+function inputLength() {
+    return userInput.value.length > 0
+}
+
+function createTodo() {
+    let li = document.createElement("li")
+    li.appendChild(document.createTextNode(userInput.value))
+    ul.appendChild(li)
+    userInput.value = ''
+
+    let deleteButton = document.createElement('button')
+    deleteButton.appendChild(document.createTextNode('X'))
+    li.appendChild(deleteButton)
+    deleteButton.addEventListener('click', deleteTodoItem)
+
+    function deleteTodoItem() {
+        li.classList.add('delete')     
+    }
+    li.onclick = function() {
+        li.classList.add('done')
+    }    
+}
+
+buttonEnter.onclick = function() {
+    if (inputLength()){
+        createTodo()
+    }
+    else {
+        alert ('Empty task!')
+    }
+}
+
+function changeListAfterKeypress(event){
+    if (inputLength() && event.which === 13){
+        createTodo()
+    }
+}
+
+userInput.addEventListener('keypress', changeListAfterKeypress)
